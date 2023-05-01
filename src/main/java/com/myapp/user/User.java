7a27760a2d25ad34631d379eb2e682c61fe03f94
dalamo20 <dalamo90@gmail.com>
2023-05-01 17:45:16 -0400

    1st commit. Adding code to repo.

---
+++ src/main/java/com/myapp/user/User.java
@@ -0,0 +1,59 @@
+package com.myapp.user;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+@Column(nullable = false, length =45, unique = true)
+    private String email;
+@Column(nullable = false, length =15)
+    private String password;
+@Column(nullable = false, length = 45, name="first_name")
+    private String firstName;
+    @Column(nullable = false, length = 45, name="last_name")
+    private String lastName;
+
+    public Integer getId() {
+        return this.id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
---
+++ src/test/java/com/myapp/UserRepositoryTests.java
@@ -0,0 +1,10 @@
+package com.myapp;
+
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+@AutoConfigureTestDatabase
+public class UserRepositoryTests {
+
+}
